ed54695ac69bb5c2de9777fe603241e9bbc0daad
ethanschreur <ethanschreur@icloud.com>
2021-02-08 12:40:05 -0500

    next commit

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 $('button').on('click', function(evt) {
-	let word = $(evt.target).previous.val();
-	console.log(word);
-	axios.get(`http://127.0.0.1:5000/submit?word=${word}`);
+	console.log($(evt.target));
+	// .previous.val();
+	// console.log(word);
+	// axios.get(`http://127.0.0.1:5000/submit?word=${word}`);
 });
 console.log('heyyyy');
